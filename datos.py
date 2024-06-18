8cd0bf439b3c9fd44bb3770e1f63b65f32390690
FranciscoGarate27 <fr.garate@duocuc.cl>
2024-06-18 22:58:05 +0000

    script de python con datos

---
+++ datos.py
@@ -0,0 +1,4 @@
+firstName = input("Cual es tu nombre? ") 
+lastName = input("Cual es tu apellido? ")  
+age = input("Cual es tu edad? ")
+print("Hola " + firstName, lastName + " tienes "+ age +" años.")
